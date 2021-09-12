54f3acaa0c260ccb3a58afeb8b243cff0e647eec
the-user-name <lenriqueromero@ucundinamarca.edu.co>
2021-09-12 17:19:26 -0500

    se agrega el proyecto

---
+++ Tercera.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tercera;
+
+/**
+ *
+ * @author luis romero
+ */
+public class Tercera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
